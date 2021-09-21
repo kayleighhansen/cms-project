891c26a3d719134008a137d7134a12d7ea726252
Kayleigh Hansen <71479913+kayleighhansen@users.noreply.github.com>
2021-09-21 11:11:45 -0600

    2/7 Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './header.component';
 
 @NgModule({
   declarations: [
