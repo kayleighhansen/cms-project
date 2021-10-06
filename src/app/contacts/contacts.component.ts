5981c178e8092a0834ff20e5ca7b0bd50e087c0f
Kayleigh Hansen <71479913+kayleighhansen@users.noreply.github.com>
2021-10-06 03:15:04 -0600

    so darn close

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contacts/contact.model';
 
 @Component({
   selector: 'app-contacts',
-  templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.css']
+  templateUrl: '/contacts.component.html',
+  styleUrls: ['/contacts.component.css']
 })
-export class ContactsComponent implements OnInit {
 
+export class ContactsComponent implements OnInit{
+  selectedContact: Contact;
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
