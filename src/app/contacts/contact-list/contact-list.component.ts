01ddc12fab2f30cb48a17eef48d291878942a696
Kayleigh Hansen <71479913+kayleighhansen@users.noreply.github.com>
2021-10-14 01:10:37 -0600

    tired

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -7,7 +7,7 @@ import { Contact } from '../contact.model';
   styleUrls: ['./contact-list.component.css']
 })
 export class ContactListComponent implements OnInit {
-  @Output() contactWasSelected = new EventEmitter<Contact>();
+  @Output() contactWasSelected = new EventEmitter<Contact>(); 
 
   contacts: Contact[] = [
     new Contact(1, 'R.Kent Jackson', 'jacksonk@byui.edu', '208-496-3771', '../../assets/images/jacksonk.jpg', 'null'),
