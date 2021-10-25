3a6031c4668f26d0e68fae0d39daec292024afc9
Kayleigh Hansen <71479913+kayleighhansen@users.noreply.github.com>
2021-10-25 14:45:33 -0600

    ...

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { Routes, RouterModule } from '@angular/router';
 
 import { ContactsComponent } from '../contacts/contacts.component';
@@ -11,20 +10,23 @@ import { MessageListComponent } from '../messages/message-list/message-list.comp
 import { DocumentEditComponent } from '../documents/document-edit/document-edit.component';
 import { DocumentDetailComponent } from '../documents/document-detail/document-detail.component';
 
-
 const appRoutes: Routes =[
   { path: '', redirectTo: '/documents', pathMatch: 'full'},
-  { path: 'contacts', component: ContactsComponent, children: [
-    { path: 'new', component: ContactEditComponent},
-    { path: ':id', component: ContactDetailComponent},
-    { path: ':id/edit', component: ContactEditComponent}
-  ]}, 
+  { path: 'contacts', component: ContactsComponent, children: 
+    [
+      { path: 'new', component: ContactEditComponent},
+      { path: ':id', component: ContactDetailComponent},
+      { path: ':id/edit', component: ContactEditComponent}
+    ]
+  }, 
   { path: 'messages', component: MessageListComponent }, 
-  { path: 'documents', component: DocumentsComponent, children: [
-    { path: 'new', component: DocumentEditComponent},
-    { path: ':id', component: DocumentDetailComponent},
-    { path: ':id/edit', component: DocumentEditComponent}
-  ]},
+  { path: 'documents', component: DocumentsComponent, children: 
+    [
+      { path: 'new', component: DocumentEditComponent},
+      { path: ':id', component: DocumentDetailComponent},
+      { path: ':id/edit', component: DocumentEditComponent}
+    ]
+  },
 ];
 
 @NgModule({
