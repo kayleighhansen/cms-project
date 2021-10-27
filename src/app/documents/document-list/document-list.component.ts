0dcca264b58b9bbfda6a0ab921390d08f651c7c8
Kayleigh Hansen <71479913+kayleighhansen@users.noreply.github.com>
2021-10-27 14:13:08 -0600

    ...

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,24 +1,33 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, OnDestroy } from '@angular/core';
 import { Document } from '../document.model';
 import { DocumentService } from '../document.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-document-list',
   templateUrl: './document-list.component.html',
   styleUrls: ['./document-list.component.css']
 })
-export class DocumentListComponent implements OnInit {
+export class DocumentListComponent implements OnInit, OnDestroy {
   documents: Document[];
+  private documentChangeSub: Subscription;
 
   constructor(private documentService: DocumentService) { 
-    this.documents = this.documentService.getDocuments();
+    //this.documents = this.documentService.getDocuments();
   } 
  
-  ngOnInit(): void {
-    
+  ngOnInit() {
+    this.documents = this.documentService.getDocuments();
+    this.documentChangeSub = this.documentService.documentChanged.subscribe();
   } 
 
   onDocumentSelected(document: Document) { 
     this.documentService.documentSelectedEvent.emit(document);
   }
+
+  ngOnDestroy(): void {
+    this.documentChangeSub.unsubscribe();
+  }
 }
+
+
