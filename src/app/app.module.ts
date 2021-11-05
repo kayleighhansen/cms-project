d84da2de26b3ff0e510893060ee0fdabc77f3a57
Kayleigh Hansen <71479913+kayleighhansen@users.noreply.github.com>
2021-11-05 00:44:19 -0600

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ContactsComponent } from './contacts/contacts.component';
@@ -42,6 +43,7 @@ import { ContactEditComponent } from './contacts/contact-edit/contact-edit.compo
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/documents/document-edit/document-edit.component.ts
+++ src/app/documents/document-edit/document-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Document } from '../document.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-document-edit',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-edit.component.css']
 })
 export class DocumentEditComponent implements OnInit {
+  @ViewChild('f') addDocumentForm: NgForm;
+
+  originalDocument: Document;
+  document: Document;
+  editMode: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  onSubmit() {
+    console.log(this.addDocumentForm);
+  }
 }
